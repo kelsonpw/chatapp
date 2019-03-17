4065fa10a6417b9404bcefdb3d1791aaaebdd5e4
Kelson Warner <kelson@null.net>
2019-03-17 11:53:30 -0700

    refactor chat input from DOM to react style

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { db } from './firebase';
 
 function ChatInputBox() {
+  const [text, setText] = useState('');
+
+  function updateMessage(event) {
+    setText(event.target.value);
+  }
+
   function postMessage(event) {
     event.preventDefault();
-    const [input] = event.target.elements;
     db.collection('channels')
       .doc('github')
       .collection('messages')
       .add({
-        text: input.value,
+        text,
         createdAt: new Date(),
       });
-    input.value = '';
+    setText('');
   }
+
   return (
     <form onSubmit={postMessage} className="ChatInputBox">
-      <input className="ChatInput" placeholder="Message #general" />
+      <input
+        className="ChatInput"
+        placeholder="Message #general"
+        value={text}
+        onChange={updateMessage}
+      />
     </form>
   );
 }
