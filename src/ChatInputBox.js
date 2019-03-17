b5f87848a3eaed965c8725335cfbe57f1979f2b4
Kelson Warner <kelson@null.net>
2019-03-17 11:49:23 -0700

    Added event listener for post message

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { db } from './firebase';
 
 function ChatInputBox() {
+  function postMessage(event) {
+    event.preventDefault();
+    const [input] = event.target.elements;
+    db.collection('channels')
+      .doc('github')
+      .collection('messages')
+      .add({
+        text: input.value,
+        createdAt: new Date(),
+      });
+    input.value = '';
+  }
   return (
-    <div className="ChatInputBox">
+    <form onSubmit={postMessage} className="ChatInputBox">
       <input className="ChatInput" placeholder="Message #general" />
-    </div>
+    </form>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
       snapshot.forEach(doc => {
         docs.push({
           ...doc.data(),
-          id: doc.id
+          id: doc.id,
         });
       });
       setChannels(docs);
@@ -34,7 +34,9 @@ function Nav() {
       </div>
       <nav className="ChannelNav">
         {channels.map(channel => (
-          <a href={`/channel/${channel.id}`}># {channel.id}</a>
+          <a key={channel.id} href={`/channel/${channel.id}`}>
+            # {channel.id}
+          </a>
         ))}
       </nav>
     </div>
