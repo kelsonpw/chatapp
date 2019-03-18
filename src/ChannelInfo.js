2a4d0492e015493ce5066cd55ccde38fd7efa94a
Kelson Warner <kelson@null.net>
2019-03-17 18:58:58 -0700

    Adding channel info

--- src/ChannelInfo.js
+++ src/ChannelInfo.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import T from 'prop-types';
 
+import useDoc from './useDoc';
+
 const propTypes = {
   channelId: T.string.isRequired,
 };
 
 function ChannelInfo({ channelId }) {
+  const channel = useDoc(`channels/${channelId}`);
   return (
     <div className="ChannelInfo">
       <div className="Topic">
-        Topic: <input className="TopicInput" value="Awesome stuff" />
+        <input className="TopicInput" defaultValue={channel && channel.topic} />
       </div>
-      <div className="ChannelName">#general</div>
+      <div className="ChannelName">#{channelId}</div>
     </div>
   );
 }
