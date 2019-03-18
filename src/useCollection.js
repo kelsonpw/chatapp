3bc848eaaf219c0622ef0d70b9983f143201fdfe
Kelson Warner <kelson@null.net>
2019-03-17 18:05:30 -0700

    adding cache to use document hook

--- src/useCollection.js
+++ src/useCollection.js
@@ -19,7 +19,7 @@ export default function useCollection(path, orderBy) {
       });
       setDocs(docs);
     });
-  }, [path]);
+  }, [path, orderBy]);
 
   return docs;
 }
--- src/useDocument.js
+++ src/useDocument.js
@@ -1,17 +1,36 @@
 import { useState, useEffect } from 'react';
 import { db } from './firebase';
 
+const cache = {};
+const pendingCache = {};
+
 export default function useDocument(path) {
-  const [doc, setDoc] = useState([]);
+  const [doc, setDoc] = useState(cache[path]);
 
   useEffect(() => {
-    db.doc(path).onSnapshot(doc => {
-      setDoc({
-        ...doc.data(),
-        id: doc.id,
-      });
+    if (doc) {
+      return;
+    }
+    let stillMounted = true;
+
+    const pending = pendingCache[path];
+
+    const promise = pending || (pendingCache[path] = db.doc(path).get());
+    promise.then(doc => {
+      if (stillMounted) {
+        const user = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        setDoc(user);
+        cache[path] = user;
+      }
     });
-  }, []);
+
+    return () => {
+      stillMounted = false;
+    };
+  }, [path]);
 
   return doc;
 }
